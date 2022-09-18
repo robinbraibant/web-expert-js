456d7c083598fc4c58ae73563f717f38f6244ef8
Robin Braibant <robin.braibant@pxl.be>
2022-09-18 15:12:30 +0200

    Opdracht 5

---
+++ opdracht05.js
@@ -0,0 +1,38 @@
+let fietsen = [
+    {
+        merk: "Kalkhoff",
+        model: "Endeavour 3.B Move",
+        prijs: 2699,
+        score: 8.7,
+    },
+    {
+        merk: "Victoria",
+        model: "E-Trekking 6.5 Wave",
+        prijs: 2599,
+        score: 8.3,
+    },
+    {
+        merk: "Pegasus",
+        model: "Siena E7F Plus",
+        prijs: 2399,
+        score: 8.1,
+    },
+    {
+        merk: "Batavus",
+        model: "Dinsdag E-Go Classic",
+        prijs: 2499,
+        score: 7.9,
+    },
+    {
+        merk: "Hercules",
+        model: "Montfoort Cruise",
+        prijs: 2499,
+        score: 7.9,
+    },
+];
+
+fietsen.forEach((fiets) => {
+    if (fiets.prijs < 2500) {
+        console.log(fiets);
+    }
+})
